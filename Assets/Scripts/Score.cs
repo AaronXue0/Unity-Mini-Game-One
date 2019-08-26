3ea5097adac507dd3abea3a0cb9deb62fff47680
Aaron Xue <aaronxue@Aaronde-MacBook-Pro.local>
2019-08-27 04:17:19 +0800

    bug fixes

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour
 {
@@ -9,12 +10,14 @@ public class Score : MonoBehaviour
     public static int score_player;
 
     private Text txt;
+    private float delay_time;
 
     // Start is called before the first frame update
     void Start()
     {
         txt = gameObject.GetComponent<Text>();
-        txt.text = score_player + " : " + score_ai;   
+        txt.text = score_player + " : " + score_ai;
+        delay_time = 0.0f;
     }
 
     // Update is called once per frame
@@ -22,5 +25,18 @@ public class Score : MonoBehaviour
     {
         //Debug.Log(score_player + " : " + score_ai);
         txt.text = score_player + " : " + score_ai;
+        if(score_ai >= 5 || score_player >= 1)
+        {
+            EndGame();
+        }
+    }
+
+    void EndGame()
+    {
+        delay_time += Time.deltaTime;
+        if(delay_time >= 2)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
     }
 }
